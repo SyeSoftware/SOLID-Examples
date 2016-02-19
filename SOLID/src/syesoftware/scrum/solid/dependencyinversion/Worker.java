23b775f285f4f2f159bf9c80ae5aee2049c986c1
Ra Acosta - Bossacorp <bossacorp@gmail.com>
2016-02-18 22:06:23 -0700

    Principio de inyeccion de dependencias

--- SOLID/src/syesoftware/scrum/solid/dependencyinversion/Worker.java
+++ SOLID/src/syesoftware/scrum/solid/dependencyinversion/Worker.java
@@ -1,3 +1,12 @@
+/**
+ * El código aqui expuesto viola el principio de inyección de depencias
+ * 
+ * ¿Qué ajuste se necesitaría hacer para que la clase Manager no dependa de la implementación?
+ * 
+ * 
+ * 
+ */
+
 package syesoftware.scrum.solid.dependencyinversion;
 
 public class Worker {
