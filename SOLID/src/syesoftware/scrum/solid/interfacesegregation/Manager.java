e5e913389c2f465c654a9666b34274403637312f
Ra Acosta - Bossacorp <bossacorp@gmail.com>
2016-02-18 22:03:20 -0700

    Integramos el principio de segregación de interfaces

--- SOLID/src/syesoftware/scrum/solid/interfacesegregation/Manager.java
+++ SOLID/src/syesoftware/scrum/solid/interfacesegregation/Manager.java
@@ -1,3 +1,10 @@
+/**
+ * Principio de segregación de interfaces
+ * La siguiente porción de código viola el principio de segregación de interfaces dado que atribuye responsabilidades que no necesitan algunas clases
+ * 
+ * ¿Que ajustes tendría que hacer a las clases para garantizar que las clases usen unicamente las interfaces que necesitan?
+ */
+
 package syesoftware.scrum.solid.interfacesegregation;
 
 public class Manager {
